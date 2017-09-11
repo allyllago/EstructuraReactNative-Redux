be3bb39a5e02c78c6a5e894b2093aac58a9311ee
allyllago <alejandrallago@gmail.com>
2017-09-11 18:06:41 -0300

    android back button working correctly

--- index.android.js
+++ index.android.js
@@ -1,28 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-
-import { AppRegistry, View, StyleSheet, Text} from 'react-native';
-import { addNavigationHelpers } from 'react-navigation';
-
+import { AppRegistry, View, StyleSheet, Text, BackHandler} from 'react-native';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
 import { Provider, connect } from "react-redux";
 
 import store from './src/store.js';
 import Home from './src/Components/home.js';
 import AppNavigator from './src/Configs/routes.js';
 
-class App extends React.Component {
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <AppNavigator navigation={addNavigationHelpers({
+//         dispatch: this.props.dispatch,
+//         state: this.props.nav,
+//       })} />
+//     );
+//   }
+// }
+
+class ReduxNavigation extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
   render() {
-    return (
-      <AppNavigator navigation={addNavigationHelpers({
-        dispatch: this.props.dispatch,
-        state: this.props.nav,
-      })} />
-    );
+    const { dispatch, nav } = this.props;
+    const navigation = addNavigationHelpers({
+      dispatch,
+      state: nav
+    });
+
+    return <AppNavigator navigation={navigation} />;
   }
 }
 
@@ -30,7 +49,7 @@ const mapStateToProps = (state) => ({
   nav: state.nav
 });
 
-const AppWithNavigationState = connect(mapStateToProps)(App);
+const AppWithNavigationState = connect(mapStateToProps)(ReduxNavigation);
 
 class Root extends React.Component {
   render() {
